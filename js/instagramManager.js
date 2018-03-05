f8a58e7b19528564c9d7afc43926c1c3b6275cc3
jsirera <judsirera@gmail.com>
2018-03-05 17:35:17 -0500

    new token

--- js/instagramManager.js
+++ js/instagramManager.js
@@ -2,7 +2,8 @@
 
 var instagramManager = {
   username: "",
-  token: '227452265.37ef44c.6aa376f9648144fe8d6f7caf06a92b90',
+  //token: '227452265.37ef44c.6aa376f9648144fe8d6f7caf06a92b90',
+  token: '227452265.45452c0.84f61d50adbd4984b238cd4f134db60a',
   requestApi: 'https://api.instagram.com/v1/locations/',
   requestToken: "",
   type: "GET",
@@ -10,6 +11,7 @@ var instagramManager = {
   requestCoordsFromLocationID: function (locationID) {
     var xhr = new XMLHttpRequest();
 
+    //console.log(this.requestApi + locationID + '?access_token=' + this.token);
     xhr.open(this.type, this.requestApi + locationID + '?access_token=' + this.token)
     xhr.onreadystatechange = function() {
       if (xhr.readyState == 4) {
